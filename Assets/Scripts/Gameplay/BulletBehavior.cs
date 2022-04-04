19b531341bce7717fd7ace30e6d1574ebf057f5c
mikeLofton <michaellofton32@gmail.com>
2022-04-04 12:49:51 -0500

    FPS

--- Assets/Scripts/Gameplay/BulletBehavior.cs
+++ Assets/Scripts/Gameplay/BulletBehavior.cs
@@ -7,11 +7,27 @@ public class BulletBehavior : MonoBehaviour
     private string _ownerTag;
     [SerializeField]
     private float _damage;
+    [SerializeField]
+    private float _lifetime;
+    private float _currentLifeTime;
+    [SerializeField]
+    private bool _destroyOnHit;
+    private Rigidbody _rigidbody;
 
     public string OwnerTag
     {
         get { return _ownerTag; }
         set { _ownerTag = value; }
+    }
+
+    public Rigidbody Rigidbody
+    {
+        get { return _rigidbody; }
+    }
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -22,6 +38,19 @@ public class BulletBehavior : MonoBehaviour
         HealthBehavior otherHealth = other.GetComponent<HealthBehavior>();
 
         if (!otherHealth)
+            return;
 
+        otherHealth.TakeDamage(_damage);
+
+        if (_destroyOnHit)
+            Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        _currentLifeTime += Time.deltaTime;
+
+        if (_currentLifeTime >= _lifetime)
+            Destroy(gameObject);
     }
 }
