018e86d1b3008f63cd407be9877bf73bac48ec6e
mikeLofton <michaellofton32@gmail.com>
2022-04-04 17:03:18 -0500

    Need to fix camera

---
+++ Assets/Scripts/Movement/PlayerMovementBehavior.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    private Rigidbody _rigidbody;
+    private Vector3 _moveDirection;
+    private Vector2 _rotationDirection;
+    private Camera _camera;
+
+    public Vector3 MoveDirection
+    {
+        get { return _moveDirection; }
+        set { _moveDirection = value; }
+    }
+
+    public Vector2 MouseDirection
+    {
+        get { return _rotationDirection; }
+        set { _rotationDirection = value; }
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _camera = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Quaternion playerRotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + MouseDirection.x, 0);
+        float cameraXRotation = Mathf.Clamp(transform.rotation.eulerAngles.x + MouseDirection.y, -89, 89);
+        Quaternion cameraRotation = Quaternion.Euler(cameraXRotation, 0, 0);
+        _rigidbody.MoveRotation(playerRotation);
+        _camera.transform.localRotation = cameraRotation;
+        Vector3 velocity = MoveDirection * _speed * Time.fixedDeltaTime;
+        _rigidbody.MovePosition(transform.position + velocity);
+    }
+}
